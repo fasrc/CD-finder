2d628a5f8dc79bee092c5d99fd815f6cb3e251e9
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2025-03-27 19:26:16 +0000

    Basic docker compose development setup

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -o errexit -o xtrace
+
+if [ ! -f /mnt/drupal.sqlite ]
+then
+  drush site:install standard --db-url=sqlite:///mnt/drupal.sqlite --account-name=admin --account-pass=admin --site-name="Drupal Site" -y
+  drush cache:rebuild
+  drush pm:enable paragraphs -y
+  drush pm:enable finder -y
+  drush cache:rebuild
+  chmod 777 /mnt/drupal.sqlite
+fi
+
+docker-php-entrypoint apache2-foreground
