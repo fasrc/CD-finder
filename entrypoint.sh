ae8de53f108747c9fbaa22dbe2d7542e45653c33
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2025-03-27 20:03:10 +0000

    Update compose setup for Drupal 10

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -o errexit -o xtrace
 
 if [ ! -f /mnt/drupal.sqlite ]
 then
-  drush site:install standard --db-url=sqlite:///mnt/drupal.sqlite --account-name=admin --account-pass=admin --site-name="Drupal Site" -y
+  drush site:install standard --db-url=sqlite://localhost//mnt/drupal.sqlite --account-name=admin --account-pass=admin --site-name="Drupal Site" -y
   drush cache:rebuild
   drush pm:enable paragraphs -y
   drush pm:enable finder -y
