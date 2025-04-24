1a851395b69724f1eefc4b4eb168bd7a3539cde9
DDA <github@dantonio.net>
2025-04-24 17:34:20 -0400

    WIP to get gulp working to change some CSS
    - Ignored `node_modules`

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,34 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+// var autoprefixer = require('gulp-autoprefixer');
 var bs = require('browser-sync').create();
 var plumber = require('gulp-plumber');
-
-gulp.task('browser-sync', ['sass'], function() {
-    bs.init({
-        proxy: "http://modularstorage.test/finder"
-    });
-});
-
-
 
 gulp.task('sass', function () {
     return gulp.src('sass/*.scss')
 	    .pipe(plumber())
 	    .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 4 versions'],
-            cascade: false
-        }))
+        // .pipe(autoprefixer({
+        //     browsers: ['last 4 versions'],
+        //     cascade: false
+        // }))
 	    .pipe(gulp.dest('css'))
 	    .pipe(bs.stream());
 });
 
+gulp.task('browser-sync', gulp.series('sass', function() {
+    bs.init({
+        proxy: "http://modularstorage.test/finder"
+    });
+}));
 
-gulp.task('watch', ['browser-sync'], function () {
+
+gulp.task('watch', gulp.series('browser-sync', function () {
     gulp.watch("sass/*.scss", ['sass']);
 
     // gulp.watch("**/*.php").on('change', bs.reload);
     gulp.watch("**/*.css").on(['add', 'change'], bs.reload);
 
-});
+}));
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', gulp.series('sass', 'watch'));
